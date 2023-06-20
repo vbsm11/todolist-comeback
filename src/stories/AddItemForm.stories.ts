b78428ef3b3e1d470fb98a6c124234b547018ec0
Vladimir <vladimir.yakimov.9811@gmail.com>
2023-06-20 06:43:11 +0300

    Complete AddItemFormStory

--- src/stories/AddItemForm.stories.ts
+++ src/stories/AddItemForm.stories.ts
@@ -1,47 +1,26 @@
-import type { Meta, StoryObj } from '@storybook/react';
-
-import { Button } from './Button';
+import type {Meta, StoryObj} from '@storybook/react';
+import {AddItemForm} from '../AddItemForm';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-const meta: Meta<typeof Button> = {
-  title: 'Example/Button',
-  component: Button,
+const meta: Meta<typeof AddItemForm> = {
+  title: 'TODOLISTS/AddItemForm',
+  component: AddItemForm,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+    addItem: {
+      description: 'Button clicked inside form',
+      action: 'clicked'
+    }
+  }
 };
 
 export default meta;
-type Story = StoryObj<typeof Button>;
+type Story = StoryObj<typeof AddItemForm>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-export const Primary: Story = {
+export const AddItemFormStory: Story = {
   // More on args: https://storybook.js.org/docs/react/writing-stories/args
-  args: {
-    primary: true,
-    label: 'Button',
-  },
 };
 
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
-};
